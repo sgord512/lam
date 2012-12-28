7ec2e859c6ed595f3a0b459f210dc94dd421d9ce
Spencer Gordon <sgord512@gmail.com>
2012-12-28 10:58:10 -0700

    improved parser to require fewer parentheses

--- Lambda/Parser.hs
+++ Lambda/Parser.hs
@@ -10,14 +10,28 @@ import Text.Parsec.String
 import Lambda.Language
 
 termP :: Parser Term
-termP =   between (char '(') (char ')') (AppC <$> termP <*> (space *> spaces *> termP))
-      <|> LamC <$> (char '\\' *> spaces *> varP) <* char '.' <*> termP
-      <|> IdC <$> varP
+termP =   try (parens $ (try lamCP) <|> appCP)
+      <|> try lamCP
+      <|> appCP          
+      <|> IdC <$> varP <* spaces                   
       <?> "term"
       
+parens = between (char '(') (char ')')
+      
+termP' :: Parser Term         
+termP' =   (try $ parens $ try lamCP <|> appCP)
+       <|> IdC <$> varP
+       <?> "term'"
+         
+appCP = foldl1 AppC <$> termP' `sepBy1` (space *> spaces) <?> "application"
+
+lamCP =   LamC <$> (char '\\' *> spaces *> varP) <* (spaces <* char '.') <*> (space *> spaces *> termP) 
+      <?> "abstraction"
+      
 varP :: Parser Var
-varP = Var <$> letter <*> optionMaybe (char '_' *> subscriptP)
-       <?> "variable"
+varP =   Var <$> letter <*> optionMaybe (char '_' *> subscriptP)
+     <?> "variable"
              
 subscriptP :: Parser Subscript             
-subscriptP = Subscript . read <$> many1 digit
+subscriptP =   Subscript . read <$> many1 digit 
+           <?> "subscript"
